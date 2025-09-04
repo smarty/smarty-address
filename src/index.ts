feb9a72a6beece954fcccc5c172527a6d624b9e9
Mike <mike.manwill@smarty.com>
2025-09-04 12:27:13 -0600

    Built out basic functionality for main application

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import {SmartyAddressConfig} from "./interfaces.ts";
+import {UiService} from "./UiServices.ts";
+import {ApiServices} from "./ApiServices.ts";
+import {EventDispatcher} from "./utils/EventDispatcher.ts";
+
+export class SmartyAddress {
+	static defaultServices: {} = {
+		UiService,
+		ApiServices,
+	};
+
+	private eventDispatcher:EventDispatcher | undefined;
+	private services:{} = SmartyAddress.defaultServices;
+
+	constructor(config: SmartyAddressConfig) {
+		this.setup(config);
+	}
+
+	setup = (config: SmartyAddressConfig) => {
+		this.eventDispatcher = new EventDispatcher();
+		this.setupServices(config.services);
+		this.eventDispatcher.dispatch("SmartyAddress.receivedSmartyAddressConfig", config);
+	}
+
+	setupServices = (services = {}) => {
+		Object.entries(services)?.forEach(([name, service]) => {
+			this.services[name] = service;
+		});
+		Object.entries(this.services).forEach(([, service]) => {
+			new service(this.eventDispatcher);
+		});
+	}
+}
