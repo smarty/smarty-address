ad8d1619b09b5754fdb8ed32810dba66600e5420
Mike <mike.manwill@smarty.com>
2025-09-04 18:02:45 -0600

    Implementation for the new way

--- src/UiServices.ts
+++ src/UiServices.ts
@@ -9,6 +9,8 @@ export const UiService = (eventDispatcher: EventDispatcher, overrideMethods = {}
 	};
 
 	const defaultMethods = {
+		configureDomElements,
+		updateAutocompleteResults,
 	};
 	const state: StateObject = {
 		eventDispatcher,
@@ -30,38 +32,119 @@ export const UiService = (eventDispatcher: EventDispatcher, overrideMethods = {}
 		...overrideMethods,
 	};
 
+	state.eventDispatcher.addEventListener("SmartyAddress.receivedSmartyAddressConfig", eventHandlerWrapper(mergedMethods.configureDomElements));
+	state.eventDispatcher.addEventListener("ApiServices.receivedAddressSuggestions", eventHandlerWrapper(mergedMethods.updateAutocompleteResults));
+};
+
+const configureDomElements: EventHandler = (event, state, setState) => {
+	const {
+		searchInputSelector,
+		streetLineSelector,
+		secondarySelector,
+		citySelector,
+		stateSelector,
+		zipcodeSelector,
+	} = event.detail;
+
+	setState("searchInputElement", searchInputSelector ? findDomElement(searchInputSelector) : null);
+	setState("streetLineInputElement", streetLineSelector ? findDomElement(streetLineSelector) : null);
+	setState("secondaryInputElement", secondarySelector ? findDomElement(secondarySelector) : null);
+	setState("cityInputElement", citySelector ? findDomElement(citySelector) : null);
+	setState("stateInputElement", stateSelector ? findDomElement(stateSelector) : null);
+	setState("zipcodeInputElement", zipcodeSelector ? findDomElement(zipcodeSelector) : null);
+
+	const searchInputElement = state.searchInputElement;
+
+	searchInputElement.addEventListener("input", state.eventHandlerWrapper(handleSearchInputOnChange));
+
+	if (!state.dropdownWrapperElement) {
+		state.dropdownWrapperElement = createDropdownWrapperElement(state.searchInputElement as HTMLInputElement);
+		searchInputElement?.parentNode?.insertBefore(state.dropdownWrapperElement, searchInputElement.nextSibling);
+	}
+
+	state.eventDispatcher.dispatch("UiService.configuredDomElements");
+}
+
+const handleSearchInputOnChange: EventHandler = (event, state) => {
+	state.eventDispatcher.dispatch("UiServices.requestedNewAddressSuggestions", {searchString: event.target?.value});
+};
+
+const createDropdownWrapperElement = () => {
+	const dropdownWrapper = document.createElement("div");
+	dropdownWrapper.classList.add("smartyAddress__suggestionsWrapperElement");
+	dropdownWrapper.appendChild(createDropDownElement());
+
+	return dropdownWrapper;
+};
+
+const createDropDownElement = () => {
+	const dropdownElement = document.createElement("ul");
+
+	dropdownElement.classList.add("smartyAddress__suggestionsElement");
+	dropdownElement.classList.add("smartyAddress__hidden");
+	dropdownElement.setAttribute('role', 'listbox');
+
+	return dropdownElement;
+};
+
+const updateAutocompleteResults:EventHandler = (event, state, setState) => {
+	const formattedSuggestions = event.detail.suggestions.map(({street_line, secondary, city, state, zipcode}) => {
+		return `<li>${street_line}, ${city}, ${state} ${zipcode}</li>`;
+	});
+	state.dropdownWrapperElement.innerHTML = formattedSuggestions.join("");
 };
 
 
+const openDropdown = () => {
 
+};
 
+const updateDropdown = () => {
 
+};
 
+const highlightAddress = () => {
 
+};
 
+const closeDropdown = () => {
 
+};
 
+const expandDropdown = () => {
 
+};
 
+const populateForm = () => {
 
+};
 
+const displayError = () => {
 
+};
 
+const displaySuccess = () => {
 
+};
 
+const updateTheme = () => {
 
+};
 
+const handleHighlightedAddressOnChange = (event) => {
 
+};
 
+const handleAddressOnSelect = (event) => {
 
+};
 
+const handleDropdownOnBlur = (event) => {
 
+};
 
-
-
-
-
-
+const findDomElement = (selector: string | undefined) => {
+	const element: HTMLElement | null = selector ? document.querySelector(selector) : null;
 
 	return element;
 };
