e461130b2f26b274df109d5c344ffdb9b3bdc4fb
Mike <mike.manwill@smarty.com>
2025-09-04 18:00:31 -0600

    Built out structure for the new way

--- src/UiServices.ts
+++ src/UiServices.ts
@@ -1,6 +1,36 @@
 import {EventDispatcher} from "./utils/EventDispatcher.ts";
+import {EventHandler, StateObject} from "./interfaces.ts";
 
+export const UiService = (eventDispatcher: EventDispatcher, overrideMethods = {}) => {
+	const eventHandlerWrapper = (eventHandler: EventHandler) => {
+		return (event: CustomEvent) => {
+			eventHandler(event, state, setState);
+		};
+	};
 
+	const defaultMethods = {
+	};
+	const state: StateObject = {
+		eventDispatcher,
+		eventHandlerWrapper,
+		searchInputElement: null,
+		streetLineInputElement: null,
+		secondaryInputElement: null,
+		cityInputElement: null,
+		stateInputElement: null,
+		zipcodeInputElement: null,
+	};
+
+	const setState = (name: string, newState: unknown) => {
+		state[name] = newState;
+	}
+
+	const mergedMethods = {
+		...defaultMethods,
+		...overrideMethods,
+	};
+
+};
 
 
 
