9467b1b1e9efa43a72b4c09b0b867c200b28c45e
Mike <mike.manwill@smarty.com>
2025-10-24 15:07:04 -0600

    We're gonna need these

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const SMARTY_LOGO_DARK_URL = "/assets/img/smarty-logo-blue.svg";
+export const SMARTY_LOGO_LIGHT_URL = "/assets/img/smarty-logo-white.svg";
+export const STYLESHEET_HREF = "/assets/styles/theme.css";
