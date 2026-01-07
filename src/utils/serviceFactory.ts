39911aa6ca683cf43663eb6e418aee2c64b77e6f
Mike <mike.manwill@smarty.com>
2026-01-07 16:43:04 -0700

    Fixed serviceFactory errors

--- src/utils/serviceFactory.ts
+++ src/utils/serviceFactory.ts
@@ -1,17 +1,17 @@
-import { ServiceDefinition, ServiceHandlersObject } from "../interfaces";
+import { ServiceDefinition, ServiceHandlersObject, ServicesObject } from "../interfaces";
 
-const allServices = {};
+const allServices: Record<number, ServicesObject> = {};
 
 export const initService = (
 	name: string,
-	{ initialState, serviceHandlers, utils }: ServiceDefinition,
+	{ initialState, serviceHandlers, utils }: ServiceDefinition<any, any>,
 	instanceId: number,
 ): ServiceHandlersObject => {
 	if (!allServices[instanceId]) allServices[instanceId] = {};
 	const instanceServices = allServices[instanceId];
 
-	const state = { instanceId, ...initialState };
-	const wrappedServiceHandlers = {};
+	const state: Record<string, any> = { instanceId, ...initialState };
+	const wrappedServiceHandlers: ServiceHandlersObject = {};
 
 	const setState = (name: string, newState: unknown) => {
 		// TODO: Make state updates more robust (or just directly modify value in the service handlers)
@@ -20,12 +20,12 @@ export const initService = (
 	const defaultProps = { state, setState, utils, services: instanceServices };
 
 	Object.entries(serviceHandlers).forEach(([actionName, action]) => {
-		wrappedServiceHandlers[actionName] = (customProps: object) => {
+		wrappedServiceHandlers[actionName] = (customProps?: any) => {
 			return action(defaultProps, customProps);
 		};
 	});
 
 	instanceServices[name] = wrappedServiceHandlers;
 
-	return instanceServices[name];
+	return instanceServices[name] as ServiceHandlersObject;
 };
