2447ef91c67499c699a4d6e8e761155f7db0e1a2
Mike <mike.manwill@smarty.com>
2026-02-06 16:49:04 -0700

    simplified config alias resolution with nullish coalescing

--- src/utils/configNormalizer.ts
+++ src/utils/configNormalizer.ts
@@ -7,23 +7,8 @@ export function normalizeConfig(config: SmartyAddressConfig): NormalizedSmartyAd
 	for (const [key, value] of Object.entries(config)) {
 		if (value === undefined) continue;
 
-		const selectorCanonical = SELECTOR_ALIASES[key];
-		if (selectorCanonical) {
-			if (normalized[selectorCanonical] === undefined) {
-				normalized[selectorCanonical] = value;
-			}
-			continue;
-		}
-
-		const filterCanonical = API_FILTER_ALIASES[key];
-		if (filterCanonical) {
-			if (normalized[filterCanonical] === undefined) {
-				normalized[filterCanonical] = value;
-			}
-			continue;
-		}
-
-		normalized[key] = value;
+		const canonicalKey = SELECTOR_ALIASES[key] ?? API_FILTER_ALIASES[key] ?? key;
+		normalized[canonicalKey] ??= value;
 	}
 
 	return normalized as unknown as NormalizedSmartyAddressConfig;
